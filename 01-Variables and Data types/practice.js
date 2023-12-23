296d841fe7017be965fb2181d01a8b11f598f917
sugyancoder <129498460+sugyancoder@users.noreply.github.com>
2023-12-23 17:25:45 +0100

    Add files via upload

---
+++ 01-Variables and Data types/practice.js
@@ -0,0 +1,16 @@
+const product = {
+    title: "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 2.7,
+};
+
+const profile = {
+    Name: "Sugyan Lama",
+    isFollowing: false,
+    followers : "200k",
+    posts : "50",
+    following : "10",
+};
+
+//Time Stamp 1:21:17
