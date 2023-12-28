90f96dc1b6ccfe943851b66e67f31a726960c6ee
sugyancoder <129498460+sugyancoder@users.noreply.github.com>
2023-12-28 16:19:34 +0100

    Add files via upload

---
+++ 04-Arrays/Script.js
@@ -0,0 +1,8 @@
+
+for( let i = 0; i < items.length; i++){
+    let offer = items[i] / 10 ;
+    items [i] -= offer;
+
+}
+
+console.log(items);
---
+++ 04-Arrays/cities.js
@@ -0,0 +1,6 @@
+// let cities = ["luzern", "Bern", "Zurich", "Baselland"]
+
+// for (let city of cities) {
+//     console.log(city.toUpperCase());
+// }
+
