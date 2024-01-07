f390c171232b4064f1bcdfd982713e151a20bf65
sugyancoder <129498460+sugyancoder@users.noreply.github.com>
2024-01-07 09:32:21 +0100

    Delete 02-Operators directory

--- 02-Operators/Ternary Operators.js
+++
@@ -1,9 +0,0 @@
-//Ternary Operators
-
-let age = 25;
-
-// let result = age >= 18 ? "adult" : "not adult"; //compact if else
-// console.log(result);
-
-// let result = age >= 18 ? console.log("adult") : console.log("not adult");
-
