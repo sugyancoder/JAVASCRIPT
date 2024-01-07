fc1fbc0e24f61e83c6c14efe630d1ed8a010a9e7
sugyancoder <129498460+sugyancoder@users.noreply.github.com>
2024-01-07 09:32:45 +0100

    Delete 04-Arrays directory

--- 04-Arrays/Script.js
+++
@@ -1,8 +0,0 @@
-
-for( let i = 0; i < items.length; i++){
-    let offer = items[i] / 10 ;
-    items [i] -= offer;
-
-}
-
-console.log(items);
--- 04-Arrays/cities.js
+++
@@ -1,6 +0,0 @@
-// let cities = ["luzern", "Bern", "Zurich", "Baselland"]
-
-// for (let city of cities) {
-//     console.log(city.toUpperCase());
-// }
-
